b7360912ac29b685e92fdf41365580bc9e802375
lakshya-dhariwal <lakshyadhariwal9@gmail.com>
2022-08-20 07:00:42 +0530

    base setup with nav

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import { Link, Route } from "wouter";
+
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Navbar />
+
+    </div>
+  );
+};
+
+export default App;
