97ae7a5fd91978da031cd5af7c40b7b4bd2c53c0
lakshya-dhariwal <lakshya@staq.com>
2023-09-12 02:07:41 +0530

    restructure client dir + add app boilerplate

---
+++ client/app/src/pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <title>DirectPay</title>
+        <link rel="icon" type="image/x-icon" href="/images/direct-pay.png" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
